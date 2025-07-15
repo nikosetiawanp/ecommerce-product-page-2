8b81267c0fd9c69718d027cda886d7054d09d882
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-15 16:23:41 +0700

    add product page

---
+++ src/components/react/ProductImage.tsx
@@ -0,0 +1,19 @@
+export default function ProductImage() {
+  return (
+    <>
+      <div className="w-full h-[300px] bg-orange-500 md:rounded-[15px] lg:hidden"></div>
+
+      <div className="hidden lg:flex flex-col gap-8 w-full max-w-[445px]">
+        <div className="h-[445px] w-[445px] bg-grey-500 rounded-[15px]"></div>
+        <div className="flex gap-8 justify-between">
+          {[1, 2, 3, 4].map((number) => (
+            <div
+              key={number}
+              className="h-[88px] w-[88px] bg-grey-500 rounded-[10px]"
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
