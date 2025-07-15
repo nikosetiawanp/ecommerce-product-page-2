51ab44afdf4617993a03a2717c9f8869bcf2de40
nikosetiawanp <nikosetiawanp@gmail.com>
2025-07-15 11:05:29 +0700

    add tailwindcss and starter files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: "https://astronaut.github.io",
   base: "ecommerce-product-page",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
